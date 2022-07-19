93c6b2e2fa995ae81b6d8e26e00334dafbb8b061
ggss-dev <gustavogss.jp@gmail.com>
2022-07-19 16:13:48 -0300

    Tela de Home

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
         background-color="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import Logo from "../assets/logo_primary.svg";
 import { Input } from "../components/Input";
@@ -6,7 +6,15 @@ import { Button } from "../components/Button";
 import { Envelope, Key } from "phosphor-react-native";
 
 export function SignIn() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(name, password);
+  }
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -19,14 +27,16 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
       <Input
         placeholder="Senha:"
         mb={8}
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   );
 }
